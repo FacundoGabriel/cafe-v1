e73c66c832e25439b36a3b0fa52acd6334b2f1f1
Facundo <facundodelacruz01@gmail.com>
2025-04-16 12:43:39 -0300

    se arregla el diseño para dispositivos más chicos

--- src/components/divisor/Divisor.jsx
+++ src/components/divisor/Divisor.jsx
@@ -6,7 +6,7 @@ export const Divisor = () => {
         <div className='container-divisor'>
             <div className='info pt-5 text-center'>
                 <h2>El mejor café, al mejor precio</h2>
-                <p className='w-50 mx-auto mt-3'>Disfrutá de una selección única de cafés especiales y sabores clásicos que te acompañan todos los días. Calidad, aroma y sabor, directo a tu taza.</p>
+                <p className='mx-auto mt-3 pb-5'>Disfrutá de una selección única de cafés especiales y sabores clásicos que te acompañan todos los días. Calidad, aroma y sabor, directo a tu taza.</p>
             </div>
         </div>
     </>
